10c26a73a37800fb65231546fcc2bdee2bd91048
someone <someone@someplace.com>
2020-09-01 06:07:08 +0200

    add job views

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Job
+from .models import Job, Category
+
+admin.site.register(Category)
 admin.site.register(Job)
